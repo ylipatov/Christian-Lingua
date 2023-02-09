9504ac9a33eef25786449a95336d10961192d311
ylipatov <yureb111@gmail.com>
2023-02-09 18:12:37 +0200

    first commit

---
+++ sources/js/script.js
@@ -0,0 +1,20 @@
+import './app/gutenberg';
+import Swiper from 'swiper/swiper-bundle';
+import {gsap} from "./app/gsap/gsap";
+import {ScrollTrigger} from "./app/gsap/ScrollTrigger";
+import {isEven, isjQuery, Coordinates, videoResize} from "./app/functions";
+
+gsap.registerPlugin(ScrollTrigger);
+
+
+(function ($) {
+
+    $('.js-open-mobile-menu').on('click', function () {
+        let $self = $(this)
+
+        $('body').toggleClass('overflow-hidden')
+        $self.toggleClass('active')
+        $('.js-header-menu').toggleClass('active')
+    })
+
+})(jQuery);
